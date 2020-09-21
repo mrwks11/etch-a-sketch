16f7601431b56f5b75a127fc9a87014cbcdad64d
mrwks11 <w.k.sinke@gmail.com>
2020-09-21 21:35:48 +0200

    Fixed canvas size, regardless # of cells + random color generator.

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-// create and add 16x16 divs
+// First time input
+let rowsInput = 16;
+let colsInput = 16;
+
 const container = document.getElementById('container');
 
+// Create a (rows * cols) grid
 function createGrid(rows, cols) {
     
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
 
-    for (let c = 0; c < (rows * cols); c++) {
+    for (let i = 0; i < (rows * cols); i++) {
         let createCell = document.createElement('div');
         createCell.classList.add('cell');
-        // createCell.classList.add(c);
-        // cell.innerText = c + 1;
         container.appendChild(createCell);
     }
+
+    // Select hovered element and change background color
+    const allCells = document.querySelectorAll('.cell');
+    Array.from(allCells).forEach(e => e.addEventListener('mouseover', function() {
+        // e.style.background = 'black';
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        e.style.background = `#${randomColor}`;
+
+    }));
+
 }
 
-createGrid(16, 16);
+createGrid(rowsInput, colsInput);
 
-// select hovered element and change background color
-const cell = document.querySelectorAll('.cell');
-Array.from(cell).forEach(e => e.addEventListener('mouseover', function() {
-    e.style.background = 'blue';
-  }));
-
-/*
 const button = document.querySelector('#btn');
 button.addEventListener('click', () => {
-    let rowsInput = prompt('How many rows?');
-    let colsInput = prompt('How many columns?');
+    container.innerHTML = '';
+    rowsInput = prompt('How many rows?');
+    colsInput = prompt('How many columns?');
     createGrid(rowsInput, colsInput);
- })
- */
+ });
